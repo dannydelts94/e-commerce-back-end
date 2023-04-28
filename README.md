5748d687ca52e1e4e6ad87ee2d04af71dd89074e
dannydelts94 <126992132+dannydelts94@users.noreply.github.com>
2023-04-28 10:46:52 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # e-commerce-back-end
+
+## Description
+
+The purpose of this project was to piut together a backend web database implementing our knowledge of MYSQL, Sequelize, and Insomnia.
+
+## Usage
+
+Use the schema.sql file in the db folder to create your database using MySQL shell commands. Use environment variables to store sensitive data, like your MySQL username, password, and database name. Run npm run seed to seed data to your database so that you can test your routes.
+
+## Credits 
+
+N/A
+
+## License
+
+MIT
+
+## Video Link and ScreenShot
+
+
+
--- models/index.js
+++ models/index.js
@@ -5,23 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+  Product.belongsTo(Category, {
+    foreignKey: "category_id"
+  })
+
 // Categories have many Products
-Category.hasMany(Product);
+  Category.hasMany(Product, {
+    foreignKey: "category_id"
+  })
+
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-});
+  Product.belongsToMany(Tag, {
+    through: ProductTag,
+   
+   foreignKey: "tag_id"
+ })
+
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-});
+  Tag.belongsToMany(Product, {
+    through: ProductTag,
+  foreignKey: "product_id"
+ })
+
 module.exports = {
   Product,
   Category,
